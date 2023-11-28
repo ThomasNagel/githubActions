6b1a08cb58ee6842a0b9059e438a7b4480eea303
ThomasNagel <tmj.nagel@gmail.com>
2023-11-28 17:28:04 +0100

    feat:"createDB" can now create database from yaml files

---
+++ createDB.sh
@@ -0,0 +1,10 @@
+id=0
+dbContent=""
+nl=$'\n'
+for file in yml/*.yml; do
+    newContent=`sed -e 's/^/  /' $file | sed "1 i\- id: $id"`
+    ((id++))
+    dbContent="$dbContent$nl$newContent"
+done
+#echo "$dbContent"
+echo "$dbContent" | yaml-to-sqlite metadata.db metadata /dev/stdin --pk id
