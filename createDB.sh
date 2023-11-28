7f44666eec2ac516a221598486842372602ad3b9
ThomasNagel <tmj.nagel@gmail.com>
2023-11-29 00:04:55 +0100

    forgot createDB.sh

--- createDB.sh
+++ createDB.sh
@@ -1,9 +1,9 @@
-dbName=$1
+dbName=$2
 id=0
 dbContent=""
 nl=$'\n'
 
-for file in yml/*.yml; do
+for file in $1/*.yml; do
     newContent=`sed -e 's/^/  /' $file | sed "1 i\- id: $id"`
     ((id++))
     dbContent="$dbContent$nl$newContent"
