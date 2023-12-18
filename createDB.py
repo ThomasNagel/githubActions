f6b4359d1f608d3184b9fa57cb37ca489ca4fdbb
ThomasNagel <tmj.nagel@gmail.com>
2023-12-18 20:51:37 +0100

    feat:'createDB.py' finished, rm:'READ.ME createDB.sh'

--- createDB.py
+++ createDB.py
@@ -3,10 +3,18 @@ import yaml
 import sqlite_utils
 import os
 
-YAMLDIR = "yml"
-PK = "id"
-
-def extractData(newKey, data, requiredFields):
+# Recursively iterate over all values in data that we want.
+# We return a flattened list of this data with new keys 
+# that sum the entire path of keys to that value.
+#
+# param:
+#   newKey, string that represents the parent keys
+#   data, the data from which we want to extract the values
+#   requiredFields, specifies the data that we are interested in
+#
+# returns:
+#   flattened list of tuples that contain a (new) key value pair
+def extractData(newKey: str, data:dict, requiredFields:dict) -> list[tuple]:
     flatData = []
     for key in requiredFields.keys():
         if key in data.keys():
@@ -21,28 +29,45 @@ def extractData(newKey, data, requiredFields):
 
     return flatData
 
-def transformData(yamlData, requiredFields):
-    jsonData = []
+# Get all the relevant data from all the yaml data
+#
+# param:
+#   yamlData, contains the data from all the yaml files that we want to process
+#   requiredFields, contains the fields that we want to extract from the yaml data
+#
+# returns:
+#    list of dicts, where each dict corresponds to one entry in the database
+def transformData(yamlData:list[tuple], requiredFields:dict) -> list[dict]:
+    dictData = []
     for id, (filename, data) in enumerate(yamlData):
-        newJson = {"id": id, "file": filename}
-        print(extractData("", data, requiredFields))
+        newDict = {"id": id, "file": filename}
+        for key, value in extractData("", data, requiredFields):
+            newDict[key] = value
         
+        dictData.append(newDict)
+    
+    return dictData
+
 
 def main():
+    # Get settings from config file
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
     # Get all yaml data
     yamlData = []
-    for filename in os.listdir(YAMLDIR):
-        fp = os.path.join(YAMLDIR, filename)
+    for filename in os.listdir(config["yamlPath"]):
+        fp = os.path.join(config["yamlPath"], filename)
         
         with open(fp, "r") as f:
             for data in yaml.safe_load_all(f):
                 yamlData.append((filename, data))
 
-    # Get the fields we want in the database
-    with open("requiredFields.json", "r") as f:
-        requiredFields = json.load(f)
+    dictData = transformData(yamlData, config["requiredFields"])
 
-    transformData(yamlData, requiredFields)
-    
+    db = sqlite_utils.Database(config["dbName"])
+    db[config["tableName"]].upsert_all(dictData, pk=config["pk"], alter=True)
+
+
 if __name__ == "__main__":
     main()
