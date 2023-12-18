5222eecdf35d59304171c65426069bddb45a4d1f
ThomasNagel <tmj.nagel@gmail.com>
2023-12-18 21:44:21 +0100

    test

--- checkPr.sh
+++ checkPr.sh
@@ -1,4 +1,5 @@
 author=$1
+echo "test"
 echo $author
-echo $(git blame yaml-files/createDB.py -p | grep "^author " | sort -u)
+#echo $(git blame yaml-files/createDB.py -p | grep "^author " | sort -u)
 echo "$(git diff)"
